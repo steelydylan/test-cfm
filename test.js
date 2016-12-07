4b278d63fea3f1b7656258b15808e3013cc2ffbb
steelydylan <ess_president@me.com>
2016-12-08 02:26:12 +0900

    add require validator

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
 var Nightmare = require('nightmare');
-var chai = require('chai')
-var expect = chai.expect;
+var assert = require("power-assert");
 var fs = require('fs');
 var pkg = require("./package.json");
 require.extensions['.html'] = function (module, filename) {
     module.exports = fs.readFileSync(filename, 'utf8');
 };
 var textField = require("./fields/text.html");
-
 
 
 describe('test text field',function(){
@@ -18,18 +16,32 @@ describe('test text field',function(){
       .goto(pkg.url)
       .type('[data-bind*="title"]', '講師名')
       .type('[data-bind*="name"]', 'teacher_name')
-      .click('[data-action*="historyClear"]')
+      .check('[data-bind*="openValidator"]')
+      .check('[data-action*="showConverter"]')
+      .click('[data-action*="addConverter(r)"]')
+      .click('[data-action*="addConverter(n)"]')
+      .click('[data-action*="addConverter(a)"]')
+      .click('[data-action*="addConverter(s)"]')
+      .click('[data-action*="addConverter(k)"]')
+      .click('[data-action*="addConverter(h)"]')
+      .click('[data-action*="addConverter(c)"]')
+      .click('[data-action*="hiddenConverter"]')
+      .check('[data-bind*="openValidator"]')
+      .select('[data-bind="validator.0.option"]',"required")
+      .type('[data-bind="validator.0.message"]',"値が入力されていません")
       .check('[data-action*="toggleCss"]')
+      .check('[data-bind*="noSearch"]')
       .click('[data-action*="submit"]')
       .evaluate(function () {
         return document.querySelector('.prettyprint').innerText.slice(0,-1);
       })
       .then(function (result) {
-        expect(result).to.equal(textField);
+        assert.equal(result,textField);
         done();
       })
       .catch(function (error) {
         console.error('Search failed:', error)
+        done();
       });
   });
 })
