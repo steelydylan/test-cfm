6edc9ec2ec1f2d043ed5a64b31067949db29782a
steelydylan <ess_president@me.com>
2016-12-08 02:35:15 +0900

    Japanese

--- test.js
+++ test.js
@@ -8,10 +8,10 @@ require.extensions['.html'] = function (module, filename) {
 var textField = require("./fields/text.html");
 
 
-describe('test text field',function(){
+describe('カスタムフィールドメーカーのテスト',function(){
   var nightMare = Nightmare(pkg.nightMare);
 
-  it('should equal to text.html',function(done){
+  it('text.htmlと同じ値になること',function(done){
       nightMare
       .goto(pkg.url)
       .type('[data-bind*="title"]', '講師名')
@@ -41,7 +41,7 @@ describe('test text field',function(){
       })
       .catch(function (error) {
         console.error('Search failed:', error)
-        done();
+        done(error);
       });
   });
 })
