f21484f3b731e8b44efc726ef2b5754f67f893fe
steelydylan <ess_president@me.com>
2016-12-08 09:22:37 +0900

    add imgtest

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ require.extensions['.html'] = function (module, filename) {
 };
 
 
-describe('カスタムフィールドメーカーのテスト',function(){
+describe('カスタムフィールドのテスト',function(){
   var textField = require("./fields/text.html");
   it('ValidatorとConverterの動作確認',function(done){
       Nightmare(pkg.nightMare)
@@ -39,6 +39,36 @@ describe('カスタムフィールドメーカーのテスト',function(){
       })
       .then(function (result) {
         assert.equal(result,textField);
+        done();
+      })
+      .catch(function (error) {
+        done(error);
+      });
+  });
+
+  var imgField = require("./fields/img.html");
+  it('画像fieldの生成',function(done){
+      Nightmare(pkg.nightMare)
+      .goto(pkg.url)
+      .select('[data-bind*="type"]','image')
+      .type('[data-bind*="title"]', '講師の画像')
+      .type('[data-bind*="name"]', 'teacher_img')
+      .select('[data-bind*="size"]','width')
+      .type('[data-bind*="normalSize"]', '100')
+      .type('[data-bind*="tiny"]', 'width')
+      .type('[data-bind*="tinySize"]', '50')
+      .type('[data-bind*="large"]', 'width')
+      .type('[data-bind*="largeSize"]', '200')
+      .type('[data-bind*="square"]', 'width')
+      .type('[data-bind*="squareSize"]', '400')
+      .check('[data-bind*="resize"]')
+      .check('[data-action*="toggleCss"]')
+      .click('[data-action*="submit"]')
+      .evaluate(function () {
+        return document.querySelector('.prettyprint').innerText.slice(0,-1);
+      })
+      .then(function (result) {
+        assert.equal(result,imgField);
         done();
       })
       .catch(function (error) {
